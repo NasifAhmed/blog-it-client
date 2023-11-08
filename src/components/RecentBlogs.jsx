61dee8f85baba3a81fded2e5710588a786b24b2f
Nasif Ahmed <nasif2ahmed@gmail.com>
2023-11-08 19:59:25 +0600

    Add recent blogs and recent comments on home page

---
+++ src/components/RecentBlogs.jsx
@@ -0,0 +1,47 @@
+import BlogCard from "../components/BlogCard";
+import BlogsSkeleton from "../components/BlogsSkeleton";
+import { useAxios } from "../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+
+const RecentBlogs = () => {
+    const axios = useAxios();
+    const response = useQuery({
+        queryKey: ["blogs", "recent", "blogcards"],
+        queryFn: async () => {
+            const res = await axios.get("/blogs?sort=-time_added");
+
+            // await new Promise((resolve) => setTimeout(resolve, 3000));
+            toast.success("Data successfully fetched!");
+            return res.data;
+        },
+    });
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {response.isLoading ? (
+                    <>
+                        <BlogsSkeleton key={1}></BlogsSkeleton>
+                        <BlogsSkeleton key={2}></BlogsSkeleton>
+                        <BlogsSkeleton key={3}></BlogsSkeleton>
+                        <BlogsSkeleton key={4}></BlogsSkeleton>
+                    </>
+                ) : (
+                    response.data
+                        .slice(0, 6)
+                        .map((data) => (
+                            <BlogCard
+                                key={data._id}
+                                payload={data}
+                                toast={toast}
+                            ></BlogCard>
+                        ))
+                )}
+                <Toaster position="top-center" reverseOrder={true}></Toaster>
+            </div>
+        </div>
+    );
+};
+
+export default RecentBlogs;
---
+++ src/components/RecentComments.jsx
@@ -0,0 +1,60 @@
+import BlogCard from "../components/BlogCard";
+import BlogsSkeleton from "../components/BlogsSkeleton";
+import { useAxios } from "../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import toast, { Toaster } from "react-hot-toast";
+import { Card, CardHeader, CardContent, CardTitle } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+const RecentComments = () => {
+    const axios = useAxios();
+    const response = useQuery({
+        queryKey: ["comments", "recent"],
+        queryFn: async () => {
+            const res = await axios.get("/comments?sort=-time_added");
+
+            // await new Promise((resolve) => setTimeout(resolve, 3000));
+            toast.success("Data successfully fetched!");
+            return res.data;
+        },
+    });
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {response.isLoading ? (
+                    <>
+                        <BlogsSkeleton key={1}></BlogsSkeleton>
+                        <BlogsSkeleton key={2}></BlogsSkeleton>
+                        <BlogsSkeleton key={3}></BlogsSkeleton>
+                        <BlogsSkeleton key={4}></BlogsSkeleton>
+                    </>
+                ) : (
+                    response.data.slice(0, 4).map((data) => (
+                        <Card key={data._id}>
+                            <CardHeader>
+                                <div className="flex justify-start items-center gap-5">
+                                    <Avatar className="w-[40px]">
+                                        <AvatarImage src={data.image_url} />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <div>
+                                        <h2 className="text-xl font-semibold">
+                                            {data.name}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </CardHeader>
+                            <CardContent>
+                                <p className="text-lg">{data.comment}</p>
+                            </CardContent>
+                        </Card>
+                    ))
+                )}
+                <Toaster position="top-center" reverseOrder={true}></Toaster>
+            </div>
+        </div>
+    );
+};
+
+export default RecentComments;
